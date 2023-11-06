455c652c5bbf602fd30e8735ef9fbd61afec0af5
manhduonghn <manhduonghn@live.com>
2023-11-07 04:42:43 +0700

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -1,7 +1,6 @@
 from src.utils import App
 from configparser import ConfigParser
 
-
 def read_urls_from_file(filename):
     urls = []
     try:
@@ -22,5 +21,5 @@ if __name__ == "__main__":
     whitelist_urls = read_urls_from_file("./lists/whitelist.ini")
     adlist_name = "DNS-Filters"
     app = App(adlist_name, adlist_urls, whitelist_urls)
-    app.delete()  # Leave script 
-    # app.run()
+    # app.delete()  # Leave script 
+    app.run()
