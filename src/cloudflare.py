b9667d5aa09ee76ef50efa76d3a8799fbc208cfc
manhduonghn <manhduonghn@live.com>
2024-06-11 22:55:02 +0700

    Update cloudflare.py

--- src/cloudflare.py
+++ src/cloudflare.py
@@ -22,8 +22,6 @@ def get_lists(name_prefix: str):
     r = session.get(
         f"https://api.cloudflare.com/client/v4/accounts/{CF_IDENTIFIER}/gateway/lists",
     )
-    if r.status_code != 200:
-        raise Exception("Failed to get Cloudflare lists")
     lists = r.json()["result"] or []
     return [l for l in lists if l["name"].startswith(name_prefix)]
 
@@ -38,8 +36,6 @@ def create_list(name: str, domains: list[str]):
             "items": [{"value": domain} for domain in domains],
         },
     )
-    if r.status_code != 200:
-        raise Exception("Failed to create Cloudflare list")
     return r.json()["result"]
 
 @retry(**tenacity_settings)
@@ -56,8 +52,6 @@ def get_firewall_policies(name_prefix: str):
     r = session.get(
         f"https://api.cloudflare.com/client/v4/accounts/{CF_IDENTIFIER}/gateway/rules",
     )
-    if r.status_code != 200:
-        raise Exception("Failed to get Cloudflare firewall policies")
     lists = r.json()["result"] or []
     return [l for l in lists if l["name"].startswith(name_prefix)]
 
@@ -77,8 +71,6 @@ def create_gateway_policy(name: str, list_ids: list[str]):
             },
         },
     )
-    if r.status_code != 200:
-        raise Exception("Failed to create Cloudflare firewall policy")
     return r.json()["result"]
 
 @retry(**tenacity_settings)
@@ -97,8 +89,6 @@ def update_gateway_policy(name: str, policy_id: str, list_ids: list[str]):
             },
         },
     )
-    if r.status_code != 200:
-        raise Exception("Failed to update Cloudflare firewall policy")
     return r.json()["result"]
 
 @retry(**tenacity_settings)
@@ -106,6 +96,4 @@ def delete_gateway_policy(policy_id: str):
     r = session.delete(
         f"https://api.cloudflare.com/client/v4/accounts/{CF_IDENTIFIER}/gateway/rules/{policy_id}",
     )
-    if r.status_code != 200:
-        raise Exception("Failed to delete Cloudflare gateway firewall policy")
     return r.json()["result"]
