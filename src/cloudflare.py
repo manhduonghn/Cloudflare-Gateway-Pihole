c48ada7b413d7f3d1ed0c9f8e63e661b325dde27
manhduonghn <manhduonghn@live.com>
2024-06-25 19:28:35 +0700

    Update cloudflare.py

--- src/cloudflare.py
+++ src/cloudflare.py
@@ -1,12 +1,12 @@
 import json
 from src import (
-    rate_limited_request,retry, send_request, retry_config 
+    rate_limited_request,retry, cloudflare_gateway_request, retry_config 
 )
 from src.colorlog import logger
 
 @retry(**retry_config)
 def get_lists(name_prefix: str):
-    status, response = send_request("GET", "/lists")
+    status, response = cloudflare_gateway_request("GET", "/lists")
     lists = response["result"] or []
     return [l for l in lists if l["name"].startswith(name_prefix)]
 
@@ -19,18 +19,18 @@ def create_list(name: str, domains: list[str]):
         "type": "DOMAIN",
         "items": [{"value": domain} for domain in domains],
     })
-    status, response = send_request("POST", "/lists", body)
+    status, response = cloudflare_gateway_request("POST", "/lists", body)
     return response["result"]
 
 @retry(**retry_config)
 @rate_limited_request
 def delete_list(name: str, list_id: str):
-    status, response = send_request("DELETE", f"/lists/{list_id}")
+    status, response = cloudflare_gateway_request("DELETE", f"/lists/{list_id}")
     return response["result"]
 
 @retry(**retry_config)
 def get_firewall_policies(name_prefix: str):
-    status, response = send_request("GET", "/rules")
+    status, response = cloudflare_gateway_request("GET", "/rules")
     lists = response["result"] or []
     return [l for l in lists if l["name"].startswith(name_prefix)]
 
@@ -47,7 +47,7 @@ def create_gateway_policy(name: str, list_ids: list[str]):
             "block_page_enabled": False,
         },
     })
-    status, response = send_request("POST", "/rules", body)
+    status, response = cloudflare_gateway_request("POST", "/rules", body)
     return response["result"]
 
 @retry(**retry_config)
@@ -63,7 +63,7 @@ def update_gateway_policy(name: str, policy_id: str, list_ids: list[str]):
             "block_page_enabled": False,
         },
     })
-    status, response = send_request("PUT", f"/rules/{policy_id}", body)
+    status, response = cloudflare_gateway_request("PUT", f"/rules/{policy_id}", body)
     return response["result"]
 
 @retry(**retry_config)
